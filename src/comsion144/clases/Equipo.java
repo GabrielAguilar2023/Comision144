8179a48d678ef01713f9814e241175e8d2e512f6
GabrielAguilar2023 <el_ayhudante@hotmail.com>
2023-03-21 16:16:02 -0300

    Aporte de estructura de las clases

--- src/comsion144/clases/Equipo.java
+++ src/comsion144/clases/Equipo.java
@@ -1,4 +1,11 @@
 package comsion144.clases;
 
 public class Equipo {
+    public String nombre;
+    public String descripcion;
+
+    public Equipo(String nombre) {
+        this.nombre = nombre;
+        this.descripcion ="Seleccion";
+    }
 }
