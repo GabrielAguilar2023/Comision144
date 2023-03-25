e565559f6007133f2c02b88b0616d9c7145a9478
GabrielAguilar2023 <el_ayhudante@hotmail.com>
2023-03-25 15:07:32 -0300

    Mejorado y comentado despues del Zoom

--- src/comision144/clases/Pronostico.java
+++ src/comision144/clases/Pronostico.java
@@ -3,56 +3,53 @@ package comision144.clases;
 import comision144.enumeracion.ResultadoEnum;
 
 public class Pronostico {
-    public Partido partido;
-    public Equipo equipo;
+    public Partido partidoSobreElQueSeApuesta;
+    public Equipo equipoSobreElQueSeApuesta;
     public ResultadoEnum resultado;
 
     // |    Equipo1    |     Gana1     |    Empata     |     Gana2     |    Equipo2    |
     // | prediccion[0] | prediccion[1] | prediccion[2] | prediccion[3] | prediccion[4] |
 
-    public Pronostico (String [] prediccion) {
-        String vectorParaPartido[]={prediccion[0],"0","0",prediccion[4],"0"};
-        this.partido = new Partido(vectorParaPartido);
-        this.equipo = new Equipo("");
+    public Pronostico (String [] prediccion,Partido partidoExistenteEnArchivoResultados) {
+        this.partidoSobreElQueSeApuesta = partidoExistenteEnArchivoResultados;
+        this.equipoSobreElQueSeApuesta = new Equipo("");
 
         if (prediccion[1].equals("X")) {
-            this.equipo.nombre = prediccion[0];
+            this.equipoSobreElQueSeApuesta.nombre = prediccion[0];
             this.resultado = ResultadoEnum.GANADOR;
         }else{
             if (prediccion[2].equals("X")){
-                this.equipo.nombre = prediccion[0];
+                this.equipoSobreElQueSeApuesta.nombre = prediccion[0];
                 this.resultado= ResultadoEnum.EMPATE;
             }else{
                 if(prediccion[3].equals("X")){
-                    this.equipo.nombre =prediccion[4];
+                    this.equipoSobreElQueSeApuesta.nombre =prediccion[4];
                     this.resultado=ResultadoEnum.GANADOR;
                 }
             }
         }
     }
-    //Coonstructor para vincular con los objetos del archivo resultados
-    public Pronostico(Partido partido, String [] prediccion) {
-        this.partido = partido;
-        this.equipo = new Equipo("");
 
-        if (prediccion[1].equals("X")) {
-            this.equipo.nombre = prediccion[0];
-            this.resultado= ResultadoEnum.GANADOR;
-        }else{
-            if (prediccion[2].equals("X")){
-                this.equipo.nombre = prediccion[0];
-                this.resultado= ResultadoEnum.EMPATE;
-            }else{
-                if(prediccion[3].equals("X")){
-                    this.equipo.nombre =prediccion[4];
-                    this.resultado=ResultadoEnum.GANADOR;
-                }
-            }
-        }
-    }
 
     public int puntos (){
+
+            if ((this.partidoSobreElQueSeApuesta.golesEquipo1) > (this.partidoSobreElQueSeApuesta.golesEquipo2)) {
+                if ((this.partidoSobreElQueSeApuesta.equipo1.equals(this.equipoSobreElQueSeApuesta.nombre)) && (this.resultado.equals(ResultadoEnum.GANADOR))) {
+                    return 1;
+                }
+            } else {
+                if ((this.partidoSobreElQueSeApuesta.golesEquipo1) < (this.partidoSobreElQueSeApuesta.golesEquipo2)) {
+                    if ((this.partidoSobreElQueSeApuesta.equipo2.nombre.equals(this.equipoSobreElQueSeApuesta.nombre)) && (this.resultado.equals(ResultadoEnum.GANADOR))) {
+                        return 1;
+                    }
+                } else {
+                    if (this.resultado.equals(ResultadoEnum.EMPATE)) {
+                        return 1;
+                    }
+                }
+            }
+
         return 0;
-        //ToDo
     }
 }
+
